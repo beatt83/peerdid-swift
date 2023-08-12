be56b23ea15efe4074a5f939c3da90c13f1af748
Goncalo Frade <goncalofrade08@gmail.com>
2023-08-12 19:22:24 +0100

    feat(resolvers): add peer did resolvers algo 0 and 2

---
+++ Sources/peerdid-swift/PeerDID/PeerDIDHelper+ResolveAlgo0.swift
@@ -0,0 +1,25 @@
+//
+//  PeerDIDHelper+ResolveAlgo0.swift
+//  
+//
+//  Created by Gonçalo Frade on 12/08/2023.
+//
+
+import Foundation
+
+extension PeerDIDHelper {
+    
+    func resolvePeerDIDAlgo0(peerDID: PeerDID, format: VerificationMaterialFormat) throws -> DIDDocument {
+        let keyStr = String(peerDID.methodId.dropFirst())
+        let decoded = try PeerDIDHelper().decodeMultibaseEcnumbasis(ecnumbasis: keyStr, format: format)
+        return DIDDocument(
+            did: peerDID.string,
+            verificationMethods: [.init(
+                id: "\(peerDID.string)#\(keyStr))",
+                controller: peerDID.string,
+                material: decoded.material
+            )],
+            services: []
+        )
+    }
+}
---
+++ Sources/peerdid-swift/PeerDID/PeerDIDHelper+ResolveAlgo2.swift
@@ -0,0 +1,42 @@
+//
+//  PeerDIDHelper+ResolveAlgo2.swift
+//  
+//
+//  Created by Gonçalo Frade on 12/08/2023.
+//
+
+import Foundation
+
+extension PeerDIDHelper {
+    
+    func resolvePeerDIDAlgo2(peerDID: PeerDID, format: VerificationMaterialFormat) throws -> DIDDocument {
+        let authenticationKeys = peerDID.algo2AuthenticationKeys
+        let agreementKeys = peerDID.algo2KeyAgreementKeys
+        let service = peerDID.algo2Service
+        
+        let authenticationVerificationMethods = try authenticationKeys
+            .map { try decodeMultibaseEcnumbasis(ecnumbasis: $0, format: format) }
+            .map { try DIDDocument.VerificationMethod(
+                did: peerDID.string,
+                ecnumbasis: $0.ecnumbasis,
+                material: $0.material
+            ) }
+        
+        let agreementVerificationMethods = try agreementKeys
+            .map { try decodeMultibaseEcnumbasis(ecnumbasis: $0, format: format) }
+            .map { try DIDDocument.VerificationMethod(
+                did: peerDID.string,
+                ecnumbasis: $0.ecnumbasis,
+                material: $0.material
+            ) }
+        
+        let documentService = try service
+            .map { try decodedPeerDIDService(did: peerDID.string, serviceString: $0) }
+        
+        return DIDDocument(
+            did: peerDID.string,
+            verificationMethods: authenticationVerificationMethods + agreementVerificationMethods,
+            services: documentService
+        )
+    }
+}
