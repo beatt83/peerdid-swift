248fff15ea3bc5329d5ec3c86c3994ec1894d627
kukgini <kukgini@users.noreply.github.com>
2024-10-31 14:20:27 +0900

    Raise didcore-swift minimum version
    Raise didcore-swift minimum version to 2.0.2. API compatibility issues with 2.0.1 and below.
    
    Signed-off-by: kukgini <kukgini@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-libp2p/swift-multibase.git", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/swift-libp2p/swift-bases.git", .upToNextMajor(from: "0.0.3")),
-        .package(url: "https://github.com/beatt83/didcore-swift.git", .upToNextMinor(from: "2.0.0"))
+        .package(url: "https://github.com/beatt83/didcore-swift.git", .upToNextMinor(from: "2.0.2"))
     ],
     targets: [
         .target(
